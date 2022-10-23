33adf0247e947b9ab0e9e0d8298dcd193a009a4e
qxb3 <qxbthree@gmail.com>
2022-10-24 00:17:19 +0800

    nvim: meow

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -68,6 +68,11 @@ require('colorizer').setup()
 vim.g.svelte_indent_script = 2
 vim.g.svelte_indent_style = 2
 
+-- vim-changesPlugin
+vim.g.changes_add_sign = '█'
+vim.g.changes_delete_sign = '█'
+vim.g.changes_modified_sign = '█'
+
 -- vim-closetag
 vim.g.closetag_filenames = '*.html,*.svelte,*.ejs'
 
