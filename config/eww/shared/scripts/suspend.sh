e8c53cbf4799ffa7cf47db6401c5a59469060825
qxb3 <qxbthree@gmail.com>
2024-01-28 21:53:21 +0800

    config: some fixes

--- config/eww/shared/scripts/suspend.sh
+++ config/eww/shared/scripts/suspend.sh
@@ -7,7 +7,7 @@ yesno_open=`echo $active_windows | grep "yesno"`
 
 # Close logout window if opened
 if [[ ! -z $logout_open ]]; then
-  eww open bar
+  eww update reveal_bar=true
   eww close logout
 fi
 
--- config/eww/shared/scripts/togglelogout.sh
+++ config/eww/shared/scripts/togglelogout.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+reveal_bar=`eww state -a | grep -oP 'reveal_bar: \K\w+'`
+if [[ "${reveal_bar}" == "true" ]]; then
+  eww update reveal_bar=false
+else
+  eww update reveal_bar=true
+fi
+
 eww update reveal_sidebar=false
 eww update reveal_notif=false
 
-source $HOME/.config/eww/shared/scripts/togglebar.sh
 eww open --toggle logout
