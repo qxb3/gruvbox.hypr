b671c00a78bce2ef1530d3590a683305d0ac8780
qxb3 <qxbthree@gmail.com>
2024-01-28 16:17:18 +0800

    config: toggle logout anim fixes

--- config/eww/shared/scripts/togglelogout.sh
+++ config/eww/shared/scripts/togglelogout.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-source $HOME/.config/eww/shared/scripts/togglebar.sh
 eww update reveal_sidebar=false
 eww update reveal_notif=false
 
+source $HOME/.config/eww/shared/scripts/togglebar.sh
 eww open --toggle logout
