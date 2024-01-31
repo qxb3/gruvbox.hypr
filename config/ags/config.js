56afa9ff7cb2c32502374cfbc994e9ec017e109b
qxb3 <qxbthree@gmail.com>
2024-01-31 21:52:15 +0800

    config: ags fix

--- config/ags/config.js
+++ config/ags/config.js
@@ -2,7 +2,7 @@ import App from 'resource:///com/github/Aylur/ags/app.js';
 import AppLauncher from './app_launcher/main.js'
 
 export default {
-  style: App.configDir + '/style.css',
+  style: App.configDir + '/out.css',
   windows: [
     AppLauncher
   ]
