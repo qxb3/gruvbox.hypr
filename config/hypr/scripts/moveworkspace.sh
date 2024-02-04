5aef968eec3c9457c802022b9e3a2100a4a8c9b6
qxb3 <qxbthree@gmail.com>
2024-02-04 16:05:47 +0800

    config: hypr: some stuff

--- config/hypr/scripts/moveworkspace.sh
+++ config/hypr/scripts/moveworkspace.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-hyprctl dispatch workspace $1
-
-eww update reveal_sidebar=false # Close sidebar
-eww update reveal_notif=false # Close notification
-eww close yesno
-
-ags -r "reveal_applauncher.setValue(false)" # Close applauncher
-
 # Close special workspace if open
 special_open=`hyprctl monitors -j | jq ".[0].specialWorkspace.name" | grep "special"`
 if [ ! -z $special_open ]; then
   hyprctl dispatch togglespecialworkspace
 fi
+
+hyprctl dispatch workspace $1
+
+eww update reveal_sidebar=false # Close sidebar
+eww update reveal_notif=false # Close notification
+eww update reveal_network=false # Close network
+eww close yesno
+
+ags -r "reveal_applauncher.setValue(false)" # Close applauncher
