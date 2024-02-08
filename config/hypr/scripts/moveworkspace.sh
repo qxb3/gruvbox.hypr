123f6276dbb6ea5671872de1a2e81fe0844ee4e5
qxb3 <qxbthree@gmail.com>
2024-02-08 23:44:19 +0800

    config: hypr stuff

--- config/hypr/scripts/moveworkspace.sh
+++ config/hypr/scripts/moveworkspace.sh
@@ -8,9 +8,10 @@ fi
 
 hyprctl dispatch workspace $1
 
+AGS_STATES=$HOME/.config/ags/.states.json
+echo `cat $AGS_STATES | jq '.app_launcher = false'` > $AGS_STATES
+
 eww update reveal_sidebar=false # Close sidebar
 eww update reveal_notif=false # Close notification
 eww update reveal_network=false # Close network
 eww close yesno
-
-ags -r "reveal_applauncher.setValue(false)" # Close applauncher
