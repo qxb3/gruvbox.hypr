297f5429fe84a319a22c5245145a29b9ab3ab984
qxb3 <qxbthree@gmail.com>
2024-02-11 03:59:57 +0800

    config: idk man

--- .config/ags/config.js
+++ .config/ags/config.js
@@ -1,5 +1,5 @@
 import App from 'resource:///com/github/Aylur/ags/app.js';
-import AppLauncher from './app_launcher/main.js'
+import AppLauncher from './applauncher/main.js'
 
 export default {
   style: App.configDir + '/out.css',
