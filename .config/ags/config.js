8f612977312630402f2efee11ee10939a24a95ca
qxb3 <qxbthree@gmail.com>
2024-02-11 12:37:44 +0800

    config: remove applauncher to build a new one

--- .config/ags/config.js
+++ .config/ags/config.js
@@ -1,12 +1,10 @@
 import App from 'resource:///com/github/Aylur/ags/app.js';
 
 import Bar from './bar/main.js'
-// import AppLauncher from './applauncher/main.js'
 
 export default {
   style: App.configDir + '/out.css',
   windows: [
-    Bar,
-    // AppLauncher
+    Bar
   ]
 }
