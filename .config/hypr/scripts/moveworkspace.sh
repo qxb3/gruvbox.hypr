f61a95ecd3153c82fd99f4ba4ad66765b20702f9
qxb3 <qxbthree@gmail.com>
2024-02-17 03:31:46 +0800

    config(hypr): close sidebar on workspace change

--- .config/hypr/scripts/moveworkspace.sh
+++ .config/hypr/scripts/moveworkspace.sh
@@ -8,6 +8,6 @@ fi
 
 hyprctl dispatch workspace $1
 
-bash ~/.config/ags/shared/scripts/applauncher.sh close
 bash ~/.config/ags/shared/scripts/changemode.sh normal
+bash ~/.config/ags/shared/scripts/sidebar.sh close
 bash ~/.config/ags/shared/scripts/wallpapers.sh close
