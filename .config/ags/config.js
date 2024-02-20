a4621d6068232b25b7da6bbfdca2f46c9437fa9b
qxb3 <qxbthree@gmail.com>
2024-02-20 11:21:43 +0800

    config(ags): remove all to start over

--- .config/ags/config.js
+++ .config/ags/config.js
@@ -1,12 +1,5 @@
-import StatusLine from './windows/statusline/StatusLine.js'
-import Sidebar from './windows/sidebar/Sidebar.js'
-import Wallpapers from './windows/wallpapers/Wallpapers.js'
-
 export default {
   style: App.configDir + '/out.css',
   windows: [
-    StatusLine,
-    Sidebar,
-    Wallpapers
   ]
 }
--- .config/ags/windows/statusline/misc/vars.js
+++
@@ -1,3 +0,0 @@
-import { state } from '../../../shared/utils.js'
-
-export const mode = state('statusline_mode', 'normal')
