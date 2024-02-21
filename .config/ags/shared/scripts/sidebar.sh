ef84277ad2dbded1e31b320be523b22cad35c942
qxb3 <qxbthree@gmail.com>
2024-02-21 12:34:37 +0800

    config(ags): sidebar.sh

--- .config/ags/shared/scripts/sidebar.sh
+++ .config/ags/shared/scripts/sidebar.sh
@@ -4,14 +4,28 @@ STATES_PATH=$HOME/.config/ags/.states.json
 
 case $1 in
   toggle)
-    echo `cat $STATES_PATH | jq '.reveal_sidebar |= not | .'` > $STATES_PATH
+    echo `cat $STATES_PATH | jq '.reveal_sidebar |= not'` > $STATES_PATH
   ;;
 
   open)
+    if [ `cat $STATES_PATH | jq '.reveal_sidebar'` == "true" ]; then
+      echo "Already opened."
+      exit 1
+    fi
+
     echo `cat $STATES_PATH | jq '.reveal_sidebar = true'` > $STATES_PATH
   ;;
 
   close)
+    if [ `cat $STATES_PATH | jq '.reveal_sidebar'` == "false" ]; then
+      echo "Already closed."
+      exit 1
+    fi
+
     echo `cat $STATES_PATH | jq '.reveal_sidebar = false'` > $STATES_PATH
+  ;;
+
+  *)
+    echo "Unknown action."
   ;;
 esac
