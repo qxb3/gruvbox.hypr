64d0d013ab7ae4c53684eefae8480d3bbd96d1a9
qxb3 <qxbthree@gmail.com>
2024-02-26 01:50:47 +0800

    config(hypr): wallpaperdaemon.sh - animation!

--- .config/hypr/scripts/wallpaperdaemon.sh
+++ .config/hypr/scripts/wallpaperdaemon.sh
@@ -3,5 +3,7 @@
 swww query
 if [ $? -eq 1 ]; then
   swww init
-  swww img ~/.config/swww/sword.jpg
+  swww img ~/.config/swww/current.set \
+    --transition-type "wipe" \
+    --transition-duration 2
 fi
