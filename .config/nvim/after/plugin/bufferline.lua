f5050f1a6c7c726c06e8d4407951947b93a42f62
qxb3 <qxbthree@gmail.com>
2024-03-20 23:49:10 +0800

    config(nvim): silince keymaps for buffer changes

--- .config/nvim/after/plugin/bufferline.lua
+++ .config/nvim/after/plugin/bufferline.lua
@@ -13,12 +13,12 @@ require('bufferline').setup({
   }
 })
 
-vim.keymap.set('n', '<C-o>', ':bprev<CR>')
-vim.keymap.set('n', '<C-p>', ':bnext<CR>')
-vim.keymap.set('n', '<C-x>', ':bd<CR>')
+vim.keymap.set('n', '<C-o>', ':bprev<CR>', { silent = true })
+vim.keymap.set('n', '<C-p>', ':bnext<CR>', { silent = true })
+vim.keymap.set('n', '<C-x>', ':bd<CR>', { silent = true })
 
-vim.keymap.set('n', '<A-1>', ':BufferLineGoToBuffer 1<CR>')
-vim.keymap.set('n', '<A-2>', ':BufferLineGoToBuffer 2<CR>')
-vim.keymap.set('n', '<A-3>', ':BufferLineGoToBuffer 3<CR>')
-vim.keymap.set('n', '<A-4>', ':BufferLineGoToBuffer 4<CR>')
-vim.keymap.set('n', '<A-5>', ':BufferLineGoToBuffer 5<CR>')
+vim.keymap.set('n', '<A-1>', ':BufferLineGoToBuffer 1<CR>', { silent = true })
+vim.keymap.set('n', '<A-2>', ':BufferLineGoToBuffer 2<CR>', { silent = true })
+vim.keymap.set('n', '<A-3>', ':BufferLineGoToBuffer 3<CR>', { silent = true })
+vim.keymap.set('n', '<A-4>', ':BufferLineGoToBuffer 4<CR>', { silent = true })
+vim.keymap.set('n', '<A-5>', ':BufferLineGoToBuffer 5<CR>', { silent = true })
