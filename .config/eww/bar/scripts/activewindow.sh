86710b4a283e2cfa54ab406ae4bf05b27bae44f6
qxb3 <qxbthree@gmail.com>
2024-04-06 02:02:30 +0800

    added eww

---
+++ .config/eww/bar/scripts/activewindow.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+function active_window() {
+  active_window=`hyprctl activewindow -j`
+  class=`echo "${active_window}" | jq -r '.initialClass'`
+  title=`echo "${active_window}" | jq -r '.title'`
+
+  active_workspace=`hyprctl monitors -j | jq '.[0].activeWorkspace.name'`
+  icon=""
+  if [[ $active_workspace == '"1"' ]]; then icon=""; fi
+  if [[ $active_workspace == '"2"' ]]; then icon="󰈹"; fi
+  if [[ $active_workspace == '"3"' ]]; then icon="󰙯"; fi
+  if [[ $active_workspace == '"4"' ]]; then icon=""; fi
+  if [[ $active_workspace == '"5"' ]]; then icon=""; fi
+
+  if [[ $class != "null" ]]; then
+    ICON='"icon": "%s"'
+    CLASS='"class": "%s"'
+    TITLE='"title": "%s"'
+
+    printf "{${ICON}, ${CLASS}, ${TITLE}}\n" "$icon" "$class" "$title"
+  else
+    ICON='"icon": "%s"'
+    CLASS='"class": "~"'
+    TITLE='"title": "~"'
+
+    printf "{${ICON}, ${CLASS}, ${TITLE}}\n" "$icon"
+  fi
+}
+
+active_window "activewindow"
+socat -u "UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" - | while read -r line; do active_window "$line"; done
---
+++ .config/eww/bar/scripts/workspaces.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+function print_workspace() {
+  workspaces=$1
+  current_workspace=$2
+
+  result=$( \
+    echo "$workspaces" | \
+    jq -c --arg active_workspace "$current_workspace" \
+      '[.[] |
+        if .name == ($active_workspace) then
+          . + {active: true}
+        else
+          . + {active: false}
+        end
+      ] | sort_by(.name) | map(select(.name != "special"))' \
+  )
+
+  echo $result
+}
+
+function check_workspaces() {
+  if [[ ${1:0:9} == "workspace" ]]; then
+    active_workspace=`hyprctl activeworkspace -j | jq -r '.name'`
+    persistent_workspaces='
+      [
+        {"name": "1", "active": false},
+        {"name": "2", "active": false},
+        {"name": "3", "active": false},
+        {"name": "4", "active": false},
+        {"name": "5", "active": false}
+      ]
+    '
+
+    print_workspace "$persistent_workspaces" "$active_workspace"
+  fi
+}
+
+check_workspaces "workspace"
+socat -u "UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" - | while read -r line; do check_workspaces "$line"; done
