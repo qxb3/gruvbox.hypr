6f108083d16f67361166b3e741303413d90f855b
qxb3 <qxbthree@gmail.com>
2024-04-14 01:15:24 +0800

    nvim: cyclist + indent-lines

--- .config/nvim/after/plugin/cyclist.lua
+++ .config/nvim/after/plugin/cyclist.lua
@@ -1,9 +1,11 @@
-vim.api.nvim_call_function('cyclist#add_listchar_option_set', {'limited', {
-    eol = '↲',
-    tab = '» ',
-    trail = '·',
-    extends = '<',
-    precedes = '>',
-    conceal = '┊',
-    nbsp = '␣',
+vim.api.nvim_call_function('cyclist#add_listchar_option_set', {'chars', {
+  eol = '↲',
+  tab = '» ',
+  trail = '',
+  extends = '<',
+  precedes = '>',
+  conceal = '┊',
+  nbsp = '␣',
 }})
+
+vim.api.nvim_call_function('cyclist#activate_listchars', {'chars'})
