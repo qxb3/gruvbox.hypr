93f8ecdcc35ea7826efc9bad506dde7fb7905084
qxb3 <qxbthree@gmail.com>
2024-04-16 12:12:37 +0800

    nvim: keybinds for switching focus thru splits

--- .config/nvim/lua/core/opts.lua
+++ .config/nvim/lua/core/opts.lua
@@ -60,6 +60,11 @@ vim.keymap.set('i', 'qq', '<Esc>', { silent = true })
 vim.keymap.set('v', 'qq', '<Esc>', { silent = true })
 vim.keymap.set('i', 'qq', '<Esc>', { silent = true })
 
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { silent = true })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { silent = true })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { silent = true })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { silent = true })
+
 vim.keymap.set('n', '<leader>so', ':source %<CR>')
 vim.keymap.set('n', '<leader>nh', ':nohl<CR>')
 vim.keymap.set('v', '<leader>y', '"+y')
