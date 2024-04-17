43da8bb416c37727f96a4840b165f83346a4124d
qxb3 <qxbthree@gmail.com>
2024-04-18 06:56:55 +0800

    nvim: auto close unamed buffers in neotree

--- .config/nvim/after/plugin/neotree.lua
+++ .config/nvim/after/plugin/neotree.lua
@@ -17,6 +17,14 @@ require('neo-tree').setup({
     {
       event = "file_opened",
       handler = function(_)
+        local buffers = vim.tbl_filter(function(bufnr)
+          return vim.api.nvim_buf_is_loaded(bufnr) and vim.api.nvim_buf_is_valid(bufnr) and vim.api.nvim_buf_get_name(bufnr) == "" and vim.api.nvim_buf_get_option(bufnr, "buflisted") and vim.api.nvim_buf_get_option(bufnr, "bufhidden") == ""
+        end, vim.api.nvim_list_bufs())
+
+        if next(buffers) ~= nil then
+          vim.api.nvim_command("silent! bdelete " .. table.concat(buffers, " "))
+        end
+
         require("neo-tree.command").execute({ action = "close" })
       end
     },
