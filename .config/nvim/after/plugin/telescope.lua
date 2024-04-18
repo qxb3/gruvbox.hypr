ef75faa44d942bf405f39a6454d13cf8fb1d475e
qxb3 <qxbthree@gmail.com>
2024-04-18 09:46:47 +0800

    nvim: more useful telescope stuff

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -10,23 +10,35 @@ require('telescope').setup({
     },
     mappings = {
       n = {
-        ['<CR>'] = actions.select_vertical
+        ['<CR>'] = actions.select_vertical,
+        ['s'] = actions.select_default
       },
       i = {
-        ['<CR>'] = actions.select_vertical
+        ['<CR>'] = actions.select_vertical,
+        ['s'] = actions.select_default
       }
     }
+  },
+  extensions = {
+    'neoclip'
   }
 })
 
-require('telescope').load_extension('neoclip')
-
 vim.keymap.set('n', '<C-x>', ':bd<CR>', { silent = true })
 
-vim.keymap.set('n', '<C-f>', builtin.find_files, {})
-vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
-vim.keymap.set('n', '<C-b>', builtin.buffers, {})
-vim.keymap.set('n', '<leader>c', ':Telescope neoclip<CR>', {})
+vim.keymap.set('n', '<C-f>', builtin.find_files, {}) -- Fuzzy find a file
+vim.keymap.set('n', '<C-g>', builtin.live_grep, {})  -- Live grep file contents
+vim.keymap.set('n', '<C-b>', builtin.buffers, {})    -- See buffers
 
-vim.keymap.set('n', '<leader>gc', builtin.git_commits, {})
-vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
+vim.keymap.set('n', '<leader>dn', builtin.diagnostics, {})   -- Project scoped lsp diagnostics
+vim.keymap.set('n', '<leader>mp', builtin.man_pages, {})     -- See Man pages
+vim.keymap.set('n', '<leader>ht', builtin.help_tags, {})     -- Vim Help tags
+vim.keymap.set('n', '<leader>sg', builtin.spell_suggest, {}) -- Spell Suggest under the cursor
+vim.keymap.set('n', '<leader>hl', builtin.highlights, {})    -- See highlights
+
+vim.keymap.set('n', '<leader>gc', builtin.git_commits, {})   -- Git commits
+vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})  -- Git branches
+
+-- Telescope plugins keymaps
+
+vim.keymap.set('n', '<leader>c', ':Telescope neoclip<CR>', { silent = true }) -- See clipboards
