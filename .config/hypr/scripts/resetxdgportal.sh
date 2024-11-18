efbdec51321952d6dee11ed6069dd06a45cbf7da
qxb3 <qxbthree@gmail.com>
2024-11-19 04:27:01 +0800

    more stuff that im doing at 3am

--- .config/hypr/scripts/resetxdgportal.sh
+++ .config/hypr/scripts/resetxdgportal.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sleep 1
+sleep 2
+
 killall xdg-desktop-portal-hyprland
 killall xdg-desktop-portal-gnome
 killall xdg-desktop-portal-kde
@@ -8,6 +9,7 @@ killall xdg-desktop-portal-lxqt
 killall xdg-desktop-portal-wlr
 killall xdg-desktop-portal
 sleep 1
+
 /usr/lib/xdg-desktop-portal-hyprland &
 sleep 2
 /usr/lib/xdg-desktop-portal &
