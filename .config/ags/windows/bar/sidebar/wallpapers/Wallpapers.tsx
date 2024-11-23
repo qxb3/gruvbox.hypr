267037263df324f9aefa1c9ef2ccbdba272b2cb2
qxb3 <qxbthree@gmail.com>
2024-11-24 03:58:17 +0800

    ags: refactor Wallpapers a bit

--- .config/ags/windows/bar/sidebar/wallpapers/Wallpapers.tsx
+++ .config/ags/windows/bar/sidebar/wallpapers/Wallpapers.tsx
@@ -3,6 +3,33 @@ import { revealSideBar, sideBarShown } from '../vars'
 
 const wallpapers = exec(`find -L ${WALLPAPERS_PATH} -iname '*.png' -or -iname '*.jpg'`).split('\n')
 
+function WallpaperList() {
+  return (
+    <>
+      {wallpapers.map(wallpaper => (
+        <button
+          className='wallpaper'
+          cursor='pointer'
+          onClick={() => {
+            exec(`ln -sf ${wallpaper} ${WALLPAPERS_PATH}/current.set`)
+            execAsync(`swww img ${wallpaper} --transition-type "wipe" --transition-duration 3`)
+
+            execAsync(`rm -rf /home/${exec('whoami')}/.cache/fastfetch/images`)
+            execAsync(`magick ${wallpaper} -gravity Center -crop 1:1 -resize 500x500 +repage ${WALLPAPERS_PATH}/current.crop`)
+
+            sideBarShown.set('home')
+            revealSideBar.set(false)
+          }}>
+          <box
+            className='img'
+            css={`background-image: url("${wallpaper}");`}
+          />
+        </button>
+      ))}
+    </>
+  )
+}
+
 export default function() {
   return (
     <box
@@ -14,26 +41,7 @@ export default function() {
           className='list'
           vertical={true}
           spacing={12}>
-          {wallpapers.map(wallpaper => (
-            <button
-              className='wallpaper'
-              cursor='pointer'
-              onClick={() => {
-                exec(`ln -sf ${wallpaper} ${WALLPAPERS_PATH}/current.set`)
-                execAsync(`swww img ${wallpaper} --transition-type "wipe" --transition-duration 3`)
-
-                execAsync(`rm -rf /home/${exec('whoami')}/.cache/fastfetch/images`)
-                execAsync(`magick ${wallpaper} -gravity Center -crop 1:1 -resize 500x500 +repage ${WALLPAPERS_PATH}/current.crop`)
-
-                sideBarShown.set('home')
-                revealSideBar.set(false)
-              }}>
-              <box
-                className='img'
-                css={`background-image: url("${wallpaper}");`}
-              />
-            </button>
-          ))}
+          <WallpaperList />
         </box>
       </scrollable>
     </box>
