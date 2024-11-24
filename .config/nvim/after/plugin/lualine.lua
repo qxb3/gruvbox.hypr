fd123a2828a19d16cf2d705070caf4e843b52810
qxb3 <qxbthree@gmail.com>
2024-11-25 03:16:19 +0800

    nvim: make lualine theme auto

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -2,7 +2,7 @@ vim.opt.laststatus = 1
 
 require('lualine').setup({
   options = {
-    theme = 'nord',
+    theme = 'auto',
     component_separators = { left = '|', right = '|' },
     section_separators = { left = '', right = '' },
     disabled_filetypes = { 'packer', 'NVimTree' }
