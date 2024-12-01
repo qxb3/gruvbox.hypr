6acc073bed585d07193a34f63ace9001dd42ad7b
qxb3 <qxbthree@gmail.com>
2024-12-01 13:40:25 +0800

    hypr: wallpaperdaemon.sh

--- .config/hypr/scripts/wallpaperdaemon.sh
+++ .config/hypr/scripts/wallpaperdaemon.sh
@@ -4,7 +4,7 @@ swww query
 if [ $? -eq 1 ]; then
   swww-daemon --format xrgb &
 
-  swww img ~/.local/state/theme/current_wallpaper \
+  swww img ~/.config/swww/current.png \
     --transition-type "wipe" \
     --transition-duration 3
 fi
