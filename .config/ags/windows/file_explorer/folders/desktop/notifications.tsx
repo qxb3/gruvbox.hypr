5bc6cd5b5053f508e90ff46c623e396f377df247
qxb3 <qxbthree@gmail.com>
2024-12-14 11:26:25 +0800

    ags: notifications done

--- .config/ags/windows/file_explorer/folders/desktop/notifications.tsx
+++ .config/ags/windows/file_explorer/folders/desktop/notifications.tsx
@@ -8,17 +8,52 @@ const notifications: Tree = {
   type: FType.CUSTOM,
   widget: (
     <box vertical={true}>
-      {bind(notifyd, 'notifications').as(notifications => {
-        return createTree({
-          type: FType.DIR,
-          name: 'notifications',
-          children: notifications.map(notification => ({
-            type: FType.FILE,
-            name: notification.get_app_name(),
-            value: notification.get_body()
-          }))
+      {bind(notifyd, 'notifications')
+        .as(notifications => {
+          const notifs = notifications.reduce<Tree[]>((acc, notification) => {
+            function findExistingNotif() {
+              return acc.findIndex(existingNotif =>
+                existingNotif.type === FType.DIR &&
+                  existingNotif.name === notification.get_app_name()
+              )
+            }
+
+            if (findExistingNotif() === -1) {
+              acc.push({
+                type: FType.DIR,
+                name: notification.get_app_name(),
+                children: []
+              })
+            }
+
+            const existingNotif = findExistingNotif()
+            if (existingNotif !== -1 && acc[existingNotif].type === FType.DIR) {
+              acc[existingNotif].children.push({
+                type: FType.CUSTOM,
+                widget: (
+                  <label
+                    label={
+                      notification.get_app_name() === 'Spotify'
+                        ? `󰝚 Playing - ${notification.get_summary()}`
+                        : notification.get_summary()
+                    }
+                    maxWidthChars={32}
+                    truncate={true}
+                  />
+                )
+              })
+            }
+
+            return acc
+          }, [])
+
+          return createTree({
+            type: FType.DIR,
+            name: 'notifications',
+            children: notifs
+          })
         })
-      })}
+      }
     </box>
   )
 }
